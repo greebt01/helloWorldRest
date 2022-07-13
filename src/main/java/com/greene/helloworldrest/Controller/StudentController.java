6b3922b67712423d9efae28f2501f1b4fac388a7
Birk <rgeebt01@yahoo.com>
2022-07-13 17:23:49 -0400

    update to query

--- src/main/java/com/greene/helloworldrest/Controller/StudentController.java
+++ src/main/java/com/greene/helloworldrest/Controller/StudentController.java
@@ -2,23 +2,18 @@ package com.greene.helloworldrest.Controller;
 
 import com.greene.helloworldrest.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
 public class StudentController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students= new ArrayList<>();
-        students.add(new Student("Ramesh", "Fadatare"));
-        students.add(new Student("Tony", "Cena"));
-        students.add(new Student("Sanjay", "Jadhav"));
-        students.add(new Student("Ram", "Jadhav"));
-        students.add(new Student("Umesh", "Fadatare"));
-        return students;
+    @GetMapping("/student/query")
+    public Student studentQueryParam(@RequestParam(name="firstName") String firstName,
+                                       @RequestParam(name="lastName") String lastName){
+        return new Student(firstName, lastName);
     }
 }
